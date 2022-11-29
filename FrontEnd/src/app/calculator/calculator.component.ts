ba6215eda0b750b74ddf24cc34dfe226d9862c36
AXm0us <74784731+Ahmed-Mohammed-11@users.noreply.github.com>
2022-11-29 11:50:56 +0200

    Update calculator.component.ts

--- FrontEnd/src/app/calculator/calculator.component.ts
+++ FrontEnd/src/app/calculator/calculator.component.ts
@@ -9,8 +9,8 @@ import { CalculatorService } from '../services/calculator.service';
 export class CalculatorComponent implements OnInit {
 
   screen: String = "0"; //content of current user input
-  firstOperand: String = "";
-  secondOperand: String = "";
+  firstOperand: String = "0";
+  secondOperand: String = "0";
   operation: String = "";
   unaryOperation: String = "";
   hist: String = ""; //operation History
@@ -21,42 +21,23 @@ export class CalculatorComponent implements OnInit {
   constructor(private calculatorService: CalculatorService) { }
   ngOnInit(): void { }
 
-  unaryOperations(unaryOp: any) {
-    if (unaryOp.target.innerText == "%") {
-      this.unaryOperation = "percent";
-    } else if (unaryOp.target.innerText == "1/x") {
-      this.unaryOperation = "inverse";
-    } else if (unaryOp.target.innerText == "x2") {
-      this.unaryOperation = "square";
-    } else if (unaryOp.target.innerText == "√ x ") {
-      this.unaryOperation = "squareRoot";
-    }
-    console.log(this.unaryOperation);
-    this.calculatorService.unaryOperations(this.screen, this.unaryOperation).subscribe(recieved => {
-      switch (this.unaryOperation) {
-        case "percent": this.hist = this.screen + "/" + "100"; break;
-        case "inverse": this.hist = "1" + "/" + this.screen; break;
-        case "square": this.hist = "sqr(" + this.screen + ")"; break;
-        case "squareRoot": this.hist = "sqrt(" + this.screen + ")"; break;
-      }
-      this.screen = recieved;
-    }, err => {
-      this.screen = err.error.text;
-      if (this.screen == "E") { this.isError = true; }
-    }
-    )
-  }
-
   handleButton(click: any) { //naming convention for events (name of action)
-    if (this.hist.charAt(this.hist.length - 1) === "=" || this.screen == "E") { this.screen = "" } //wipe out screen and start over
+    //wipe out screen and start over
+    if (this.hist.charAt(this.hist.length - 1) === '=' || this.screen == "E") {
+      this.hist = "";
+      this.screen = ""
+    }
+    console.log(this.hist);
     this.isError = false;
     //get the data of the pressed button and append it on screen
-    if (this.screen.charAt(0) == "0") { this.screen = this.screen.replace(this.screen.charAt(0), ""); }
+    if (this.screen.substring(0, 1) === '0') {
+      this.screen = this.screen.replace(this.screen.charAt(0), "");
+    }
     this.screen += click.target.innerText;
   }
 
   handleOperation(operation: any) {
-    if (this.screen == "") { //replace operation (undo operation)
+    if (this.screen == "" && this.screen != "0") { //replace operation (undo operation)
       this.operation = operation.target.innerText;
       this.hist = this.hist.replace(/.$/, operation.target.innerText);
       return;
@@ -69,28 +50,58 @@ export class CalculatorComponent implements OnInit {
     this.screen = "";
   }
 
+  unaryOperations(unaryOp: any) {
+    //this if block just to make unaryOperation text more stable "innerHTML has some errors :)"
+    if (unaryOp.target.innerText == "%") {
+      this.unaryOperation = "percent";
+    } else if (unaryOp.target.innerText == "1/x") {
+      this.unaryOperation = "inverse";
+    } else if (unaryOp.target.innerText == "x2") {
+      this.unaryOperation = "square";
+    } else if (unaryOp.target.innerText == "√ x ") {
+      this.unaryOperation = "squareRoot";
+    }
+
+    this.calculatorService.unaryOperations(this.screen, this.unaryOperation).subscribe(recieved => {
+      //set the history sub screen according to the operation
+      switch (this.unaryOperation) {
+        case "percent": this.hist = this.screen + "/" + "100"; break;
+        case "inverse": this.hist = "1" + "/" + this.screen; break;
+        case "square": this.hist = "sqr(" + this.screen + ")"; break;
+        case "squareRoot": this.hist = "sqrt(" + this.screen + ")"; break;
+      }
+      this.screen = recieved;
+    }, err => {
+      this.screen = err.error.text;
+      this.handleError();
+      if (this.screen == "E") { this.isError = true; } //to disable some buttons
+    }
+    )
+  }
+
   binaryOperations(result: any) {
     this.isError = false;
     if (this.hist.charAt(this.hist.length - 1) === "=") { return; }
     this.secondOperand = this.screen;
     //send http get request to calculate at server side and get whether the result or error
     this.calculatorService.binaryOperations(this.firstOperand, this.operation, this.secondOperand).subscribe(recieved => {
-      this.result = recieved
+      this.result = recieved;
       this.hist = String(this.firstOperand) + this.operation + String(this.secondOperand) + "=";
       this.screen = this.result;
+      console.log(this.hist);
     }, err => {
-      this.result = err.error.text;
-      this.screen = this.result;
-      this.hist += "0";
-      if (this.result == "E") { this.isError = true; }
+      this.screen = err.error.text;
+      this.handleError();
+      if (this.screen == "E") { this.isError = true; } //to disable some buttons
     });
+
   }
 
   //C and CE buttons
   reset(reset: any) {
     this.isError = false;
     this.screen = "0"; //clear the screen 
-    this.hist = ""; //clear the history
+    this.hist = "";    //clear the history
     this.firstOperand = "";
     this.operation = "";
     this.secondOperand = "";
@@ -117,5 +128,13 @@ export class CalculatorComponent implements OnInit {
     if (this.screen.includes(".")) { return; }
     this.screen += '.';
     //add floating point 
+  }
+
+  //what to do when error occurs
+  handleError() {
+    this.firstOperand = "";
+    this.operation = "";
+    this.secondOperand = "";
+    this.hist = "";
   }
 }
